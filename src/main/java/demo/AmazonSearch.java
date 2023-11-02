670406ed93996445e464b83d8b82918d2b1b50e6
Shailesh Harsure <shailesh.harsure1@gmail.com>
2023-11-02 19:33:37 +0530

    Automate Search amazon case

---
+++ src/main/java/demo/AmazonSearch.java
@@ -0,0 +1,49 @@
+package demo;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import net.bytebuddy.asm.Advice.Enter;
+
+public class AmazonSearch {
+    
+    ChromeDriver driver;
+
+       public AmazonSearch()
+    {
+        
+        System.out.println("Constructor: TestCases");
+        WebDriverManager.chromedriver().timeout(30).setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+    }
+    public void endTest()
+    {
+        System.out.println("End Test: TestCases");
+        driver.close();
+        driver.quit();
+
+    }
+    public  void testCase01(){
+       
+        // Lauch chrome browser by entering URL  www.google.com
+        driver.get("https://www.google.com");
+        
+        WebElement searchbox = driver.findElement(By.name("q"));
+        searchbox.sendKeys("amazon");
+        searchbox.sendKeys(Keys.RETURN);
+        
+        WebElement ele = driver.findElement(By.xpath("//a[contains(@href,'amazon.in') or contains(@href,'amazon.com')]"));
+        if (ele.isDisplayed()) {
+            System.out.println("Element is  visible");
+        } else
+            System.out.println("Element not visible");
+
+        }
+    }
